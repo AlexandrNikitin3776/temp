9b250afbdd55a0afe838ec0256b98b2e745f9b7a
Alexander <43119129+AlexandrNikitin3776@users.noreply.github.com>
2020-11-07 00:42:31 +0300

    Change parentheses parsing and getnode function

--- temp.py
+++ temp.py
@@ -15,10 +15,25 @@ def parse(query: str) -> dict:
     """
     querylist = re.split(r"\s*(\(|\)|AND|OR)\s*", query)
     querylist = list(filter(lambda a: a != '', querylist))
-    return getnode(querylist)
+
+    parenthesesstack = [[]]
+    for item in querylist:
+        if item == '(':
+            parenthesesstack.append([])
+        elif item == ')':
+            laststackitem = parenthesesstack.pop(-1)
+            parenthesesstack[-1].append(getnode(laststackitem))
+        else:
+            parenthesesstack[-1].append(item)
+    laststackitem = parenthesesstack.pop(-1)
+    assert parenthesesstack == [], 'Missed closing parenthesis.'
+    return getnode(laststackitem)
 
 
 def getleaf(query: str):
+    # TODO: Use re tokens
+    # TODO: Use logic expression in re
+
     leafpattern = re.compile(r'(?P<id>.+)\s*(?P<op>(=|!=|>=|<=|>|<))\s*(?P<literal>.+)')
     leafmatch = re.match(leafpattern, query)
 
@@ -26,15 +41,15 @@ def getleaf(query: str):
     assert leafmatch is not None, assertionmessage
 
     # literal type converting
-    literalmatch = re.match(r'((?P<int>\d+)|(?P<float>\d*.\d+)|(?P<str>\".+\"))', leafmatch.group("literal"))
+    literalmatch = re.match(r'((?P<int>\d+)|(?P<float>\d*.\d+)|\"(?P<str>[^\"]+)\"|(?P<err>.+))', leafmatch.group("literal"))
     if literalmatch.group('int'):
-        literal = int(leafmatch.group("literal"))
+        literal = int(literalmatch.group("int"))
     elif literalmatch.group('float'):
-        literal = float(leafmatch.group("literal"))
+        literal = float(literalmatch.group("float"))
     elif literalmatch.group('str'):
-        literal = leafmatch.group("literal")
-    else:
-        raise Exception(f"Leaf '{query}' literal isn't int, float or str.")
+        literal = literalmatch.group("str")
+    elif literalmatch.group('err'):
+        raise Exception(f"In leaf '{query}' literal {leafmatch.group('literal')} isn't int, float or str.")
 
     return {
          'type': 'leaf',
@@ -44,39 +59,34 @@ def getleaf(query: str):
     }
 
 
-def getnode(querylist: list):
-    i = 0
-    while i < len(querylist):
-        if querylist[i] == '(':
-            pass
-        elif querylist[i] == ')':
-            pass
-        elif querylist[i] in ['AND', 'OR']:
-            return {
+def getnode(query):
+    node = {
                 'type': 'node',
-                'op': querylist[i],
-                'left': querylist[:i],
-                'right': getnode(querylist[i + 1:]),
-            }
+                'op': None,
+                'left': None,
+                'right': None,
+    }
+    for i in range(len(query))[::-1]:
+        if query[i] in ['AND', 'OR']:
+            node['op'] = query[i]
+            continue
+        if isinstance(query[i], dict):
+            item = query[i]
         else:
-            querylist[i] = getleaf(querylist[i])
-        i += 1
-
-
-
-# def getnode(query:str):
-#     nodepattern = re.compile(r'(?P<left>\b((?!(AND|OR)))\b\S+)\s+(?P<op>(AND|OR))\s+(?P<right>.+)')
-#     nodematch = re.match(nodepattern, query)
-
-#     if nodematch is None:
-#         return getleaf(query)
-
-#     return {
-#         'type': 'node',
-#         'op': nodematch.group("op"),
-#         'left': getleaf(nodematch.group("left")),
-#         'right': getnode(nodematch.group("right")),
-#     }
+            item = getleaf(query[i])
+        if node['right']:
+            node['left'] = item
+            if i > 0:
+                prevnode = node
+                node = {
+                            'type': 'node',
+                            'op': None,
+                            'left': None,
+                            'right': prevnode,
+                }
+        else:
+            node['right'] = item
+    return node
 
 
    # Необходимо написать функцию, пользуясь любыми библиотеками или без них.
@@ -85,18 +95,20 @@ def getnode(querylist: list):
    # При использовании сторонних библиотек парсинга необходимо написать 5-10 ассертов,
    # т.е. проверить как можно больше граничных случаев.
 
-# assert parse('Пол="М" AND (Возраст>25 OR Стаж>.5)') == {
-#    'type': 'node', 'op': 'AND',
-#    'left': {'type': 'leaf', 'op': '=', 'id': 'Пол', 'literal': "М"},
-#    'right': {
-#        'type': 'node', 'op': 'OR',
-#        'left': {'type': 'leaf', 'op': '>', 'id': 'Возраст', 'literal': 25},
-#        'right': {'type': 'leaf', 'op': '>', 'id': 'Стаж', 'literal': 0.5}
-#    }
-# }
+assert parse('Пол="М" AND (Возраст>25 OR Стаж>.5)') == {
+    'type': 'node', 'op': 'AND',
+    'left': {'type': 'leaf', 'op': '=', 'id': 'Пол', 'literal': "М"},
+    'right': {
+        'type': 'node', 'op': 'OR',
+        'left': {'type': 'leaf', 'op': '>', 'id': 'Возраст', 'literal': 25},
+        'right': {'type': 'leaf', 'op': '>', 'id': 'Стаж', 'literal': 0.5}
+    }
+}
 
-print(parse('Пол="М" AND (Возраст>25 OR Стаж>.5)'))
+
+
+# print(parse('Пол="М" AND (Возраст>25 OR Стаж>.5)'))
 # print(getleaf('Пол="М"'))
 # print(getleaf('Возраст >= 25'))
 # print(getleaf('Стаж>""5"'))
-# print(getnode('Возраст>25 OR Стаж>.5 AND Имя="Василий"'))
+# print(parse('Возраст>25 OR Стаж>.5 AND Имя="Василий"'))
